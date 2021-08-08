ca5c72af9b90d9de41233c7743abc7f0ae16699c
LKlemens <klemens.lukaszczyk@gmail.com>
2021-08-08 19:22:41 +0200

    add creating admin account to seeds.ex

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias EmployeeRewardApp.Accounts
+
+admin = %{
+  email: "changeme@changeme",
+  password: "strongadmin",
+  role: "admin"
+}
+
+Accounts.register_user(admin)
