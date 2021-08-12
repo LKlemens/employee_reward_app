ca863b406e946820c6ce20b60fbfffc21b05d597
LKlemens <klemens.lukaszczyk@gmail.com>
2021-08-12 09:46:14 +0200

    add current_user to RewardLive.Index - enable edit only for :admin role

--- lib/employee_reward_app_web/live/reward_live/index.ex
+++ lib/employee_reward_app_web/live/reward_live/index.ex
@@ -2,10 +2,22 @@ defmodule EmployeeRewardAppWeb.RewardLive.Index do
   use EmployeeRewardAppWeb, :live_view
 
   alias EmployeeRewardApp.Reward
+  alias EmployeeRewardApp.Accounts
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, :users, Reward.list_users())}
+  def mount(_params, %{"user_token" => token} = _session, socket) do
+    {:ok,
+     socket
+     |> assign_user(token)
+     |> assign_users()}
+  end
+
+  def assign_user(socket, token) do
+    assign_new(socket, :current_user, fn -> Accounts.get_user_by_session_token(token) end)
+  end
+
+  def assign_users(socket) do
+    assign(socket, :users, Reward.list_users())
   end
 
   @impl true
