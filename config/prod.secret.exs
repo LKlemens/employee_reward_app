c33a7d69cc65a70b3ac90b5730da103fa4467181
LKlemens <klemens.lukaszczyk@gmail.com>
2021-08-18 20:28:31 +0200

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :employee_reward_app, EmployeeRewardApp.Repo,
-  # ssl: true,
+  ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
--- lib/employee_reward_app_web/endpoint.ex
+++ lib/employee_reward_app_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule EmployeeRewardAppWeb.Endpoint do
   ]
 
   socket "/socket", EmployeeRewardAppWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
